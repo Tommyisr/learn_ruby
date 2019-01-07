22f353498b438d22853a512ef81df4a4bf674caa
Tommy Bogdanovich <tommybogdanovich@gmail.com>
2019-01-07 21:17:28 +0200

    05_book completed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,30 @@
 class Book
-# write your code here
+    # write your code here
+    attr_reader :title
+
+
+    def title=(str)
+        arr = []
+        huy = %w(and the over in of a an)
+        str.split(' ').each_with_index do |x, i|
+            if huy.include? x and i!=0
+                arr << x
+            else arr << x.capitalize
+            end
+            # arr << x.capitalize
+        end
+        @title = arr.join(' ')
+        # arr.join(' ')
+        # arr.join(' ')
+        # @title = str.capitalize
+        # puts 'HUY'
+    end
+
+
 end
+
+
+# @book = Book.new
+# @book.title = 'inferno huy sobaka'
+# @book.title = 'pistun'
+# @book.title
